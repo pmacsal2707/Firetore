06f8f8283f667eead1952e9665722dbea6d287f3
Pablo <pmacsal2707@g.educaand.es>
2025-02-12 14:06:19 +0100

    cambios

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCT1NglWcIgucQMm6XapqAcA0pdsVXtGa8",
+    authDomain: "proyactoalimentos.firebaseapp.com",
+    projectId: "proyactoalimentos",
+    storageBucket: "proyactoalimentos.firebasestorage.app",
+    messagingSenderId: "118085880525",
+    appId: "1:118085880525:web:e5077996233a409a76b784"
+  }
 };
